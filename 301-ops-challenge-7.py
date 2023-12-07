eb76c06aedb6db21a8c6204ed6c1d373357a5e7c
Cody <skalyx86@gmail.com>
2023-12-06 19:54:43 -0800

    Eighteth ops challenge

---
+++ 301-ops-challenge-7.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+# Declaring arrays
+
+vegetables = ["onion", "zucchini", "squash", "lettuce", "cucumber", "carrot", "corn", "radish", "beats", "eggplant"]
+
+# prints fourth item in the array
+print (vegetables[3])
+
+# prints sixth through tenth item in the list
+
+print (vegetables[5:9])
+
+# changees sixth item to onion
+vegetables[5] = "onion"
+
+# prints the new list
+
+print (vegetables[5])
